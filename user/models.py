4d7bd41077d7b766f0b9755372ed6ef557eb6479
younghoon <digitcomgisul@gmail.com>
2021-05-27 10:53:22 +0900

    ADMIN 활용
    - 생성한 앱을 admin에 반영
    - admin에서 가독성좋게 보여주도록 수정

--- user/models.py
+++ user/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class User(models.Model):
-    usrename = models.CharField(max_length=32, verbose_name='사용자명')
+    username = models.CharField(max_length=32, verbose_name='사용자명')
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
+
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = 'user'
+        verbose_name = '일반 사용자'
+        verbose_name_plural = '일반 사용자'
