2d87b50a2bd15e9ca2ff5cad4ac4378b529c6934
younghoon <digitcomgisul@gmail.com>
2021-05-28 09:35:21 +0900

    회원가입
    - 입력필드 validation

--- user/views.py
+++ user/views.py
@@ -10,13 +10,15 @@ def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
     elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        re_password = request.POST['re-password']
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
 
         res_data = {}
 
-        if password != re_password:
+        if not (username and password and re_password):
+            res_data['error'] = '모든 입력값을 입력해야 합니다.'
+        elif password != re_password:
             res_data['error'] = '비밀번호와 비밀번호 확인이 일치하지 않습니다!'
         else:
             user = User(
