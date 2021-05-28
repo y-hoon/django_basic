24ca00955f92623dac7781594860912983bfbee6
younghoon <digitcomgisul@gmail.com>
2021-05-28 09:06:03 +0900

    회원가입
    - 저장 기능 구현

--- user/views.py
+++ user/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        re_password = request.POST['re-password']
+
+        user = User(
+            username=username,
+            password=password
+        )
+
+        user.save()
+
+        return render(request, 'register.html')
+
