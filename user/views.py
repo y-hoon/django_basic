2fe1b389c051d18e667e776989a09bbdcb32dd84
younghoon <digitcomgisul@gmail.com>
2021-05-31 08:24:58 +0900

    회원가입
    - 이메일 field 추가

--- user/views.py
+++ user/views.py
@@ -11,18 +11,20 @@ def register(request):
         return render(request, 'register.html')
     elif request.method == 'POST':
         username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
         re_password = request.POST.get('re-password', None)
 
         res_data = {}
 
-        if not (username and password and re_password):
+        if not (username and useremail and password and re_password):
             res_data['error'] = '모든 입력값을 입력해야 합니다.'
         elif password != re_password:
             res_data['error'] = '비밀번호와 비밀번호 확인이 일치하지 않습니다!'
         else:
             user = User(
                 username=username,
+                useremail=useremail,
                 password=make_password(password)
             )
 
