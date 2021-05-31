be894b3c75b2c7d23ec33b75306ee433cc7995be
younghoon <digitcomgisul@gmail.com>
2021-06-01 08:13:09 +0900

    login 기능
    login validation - 입력필드 체크, 비밀번호 불일치

--- user/views.py
+++ user/views.py
@@ -1,12 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from .models import User
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+        if not (username and password ):
+            res_data['error'] = '모든 입력값을 입력해야 합니다.'
+        else:
+            user = User.objects.get(username=username)
+            if check_password(password, user.password):
+                # 비밀번호 일치, 로그인 성공
+                pass
+            else:
+                res_data['error'] = '비밀번호가 일치하지 않습니다.'
+
+        return render(request, 'login.html', res_data)
 
 
 def register(request):
