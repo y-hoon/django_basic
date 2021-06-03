857e19103f205da47101942b57245489878e5ae5
younghoon <digitcomgisul@gmail.com>
2021-06-04 08:47:11 +0900

    Form 활용
    form validation 처리
    세션에 데이터 추가

--- user/views.py
+++ user/views.py
@@ -26,7 +26,14 @@ def logout(request):
 
 
 def login(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+
     return render(request, 'login.html', {'form': form})
 
 
