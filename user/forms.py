1aaf4d5e7f9944a4d99ac2e7cccec8ed9a1fc14a
younghoon <digitcomgisul@gmail.com>
2021-06-03 09:26:01 +0900

    Form 활용
    필드 label 값 적용, 비밀번호 필드 적용
    form 필드별 설정

--- user/forms.py
+++ user/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=32)
-    password = forms.CharField()
+    username = forms.CharField(max_length=32, label="사용자 이름")
+    password = forms.CharField(widget=forms.PasswordInput, label="비밀번호")
