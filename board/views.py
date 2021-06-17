b6e99e698ae789e5d6717c3f193aab6d8abcd252
younghoon <digitcomgisul@gmail.com>
2021-06-18 08:51:00 +0900

    게시판
    목록조회 기능 구현

--- board/views.py
+++ board/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Board
 
 
 # Create your views here.
 def board_list(request):
-    return render(request, 'board_list.html')
+    boards = Board.objects.all().order_by('-id')
+    return render(request, 'board_list.html', {'boards': boards})
