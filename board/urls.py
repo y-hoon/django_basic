619a72b365950db1074ccb8f0629db6e6c3003a9
younghoon <digitcomgisul@gmail.com>
2021-06-22 08:37:39 +0900

    게시판
    쓰기 기능 구현

--- board/urls.py
+++ board/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('list/', views.board_list),
+    path('write/', views.board_write),
 ]
--- board/views.py
+++ board/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
+from .forms import BoardForm
+from user.models import User
 
 
-# Create your views here.
+# create your views here.
+
+
 def board_list(request):
     boards = Board.objects.all().order_by('-id')
     return render(request, 'board_list.html', {'boards': boards})
+
+
+def board_write(request):
+    if request.method == 'post':
+        form = BoardForm(request.post)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            user = User.objects.get(pk=user_id)
+
+            board = Board()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+            board.writer = user
+            board.save()
+
+            return redirect('/board/list/')
+    else:
+        form = BoardForm()
+
+    return render(request, 'board_write.html', {'form': form})
