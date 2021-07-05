05ee2d76fe7bb8304d6b6d00a0ba74b3bdcd3f7b
younghoon <digitcomgisul@gmail.com>
2021-07-06 08:34:16 +0900

    pythonanywhere 배포 연습
    배포위한 설정

--- django_basic/settings.py
+++ django_basic/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*!nf+_(ne*8m7a3kaqm2sp0rx)ksv$rm0e7!993w_g6msm1%!v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'digitcom.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -122,9 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
